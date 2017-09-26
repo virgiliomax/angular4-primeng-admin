4f8a3b2fdbff5ebee5786347b09b87d5ac7bea4d
Virgilio Maximiliano Santana Rodrigues <virgiliomaxx@gmail.com>
2017-09-26 10:15:56 -0300

    Update tree.module.ts

--- src/app/tree/tree.module.ts
+++ src/app/tree/tree.module.ts
@@ -3,26 +3,26 @@ import { CommonModule } from '@angular/common';
 import { RouterModule} from "@angular/router";
 import {
   PanelMenuModule,
-  TabMenuModule,   //tabMenu
+  TabMenuModule,   // tabMenu
   AutoCompleteModule,
-  ButtonModule,  //Button
-  PanelModule,     //容器
+  ButtonModule,  // Button
+  PanelModule,     // Painel
   InputTextModule,
-  DataTableModule,  //表格
-  DialogModule,   //浮层
+  DataTableModule,  // Tabela de Dados
+  DialogModule,   // Modal
   SplitButtonModule,
-  TabViewModule,  //table
-  AccordionModule,//手风琴
+  TabViewModule,  // table
+  AccordionModule,// Accordion
   SharedModule,
   CalendarModule,
-  GrowlModule,//弹出框
-  MultiSelectModule,//多选
-  ListboxModule,//多选框
-  DropdownModule,//下拉
-  CheckboxModule,//多选
-  MessagesModule,//Messages
-  PaginatorModule,//分页
-  ConfirmDialogModule,//删除弹出框
+  GrowlModule, // Pop-up
+  MultiSelectModule, // Multi-seleção
+  ListboxModule,// Caixa de Seleção
+  DropdownModule,// Drop-down
+  CheckboxModule,// Checkbox
+  MessagesModule, // Messages
+  PaginatorModule,// Paginação
+  ConfirmDialogModule,// Modal Confirmação
   InputTextareaModule,// InputTextarea
   OverlayPanelModule,
   TooltipModule, TreeTableModule,
