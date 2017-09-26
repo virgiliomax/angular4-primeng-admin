35ea7880445ba8d610e5435a86629765da5e9dce
Virgilio Maximiliano Santana Rodrigues <virgiliomaxx@gmail.com>
2017-09-26 11:07:03 -0300

    Update charts.component.ts

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -16,16 +16,16 @@ export class ChartsComponent implements OnInit {
 
   ngOnInit() {
     this.data = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho'],
       datasets: [
         {
-          label: 'My First dataset',
+          label: 'Meu Primeiro Dataset',
           backgroundColor: '#42A5F5',
           borderColor: '#1E88E5',
           data: [65, 59, 80, 81, 56, 55, 40]
         },
         {
-          label: 'My Second dataset',
+          label: 'Meu Segundo Dataset',
           backgroundColor: '#9CCC65',
           borderColor: '#7CB342',
           data: [28, 48, 40, 19, 86, 27, 90]
@@ -33,6 +33,6 @@ export class ChartsComponent implements OnInit {
       ]
     }
   }
-  msgs: Message[] = [];                                  //消息
+  msgs: Message[] = [];                                  // Mensagem
   data: any;
 }
